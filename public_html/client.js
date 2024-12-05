d48b68b144927f6451cad8ef02dbc71a298d872f
emiliano <emipaz1975@hotmail.com>
2024-12-05 13:42:53 -0300

    alerta por inactividad

--- public_html/client.js
+++ public_html/client.js
@@ -1,7 +1,8 @@
 const socket = new WebSocket('ws://localhost:12345');
 const time = 60000 // 1 minuto
 let session_id = null;
-
+let inactivityTimeout;
+let alertTimeout; 
 socket.onopen = () => {
     console.log('Conexcion exitosa');
 };
@@ -18,11 +19,7 @@ socket.onmessage = (event) => {
     responseArea.innerHTML += `<p>Respuesta: ${data.reply}</p>`;
 
     clearTimeout(inactivityTimeout);
-    inactivityTimeout = setTimeout(() => {
-        console.log('Cerrando conección por inactividad');
-        sendCloseSessionMessage();
-        socket.close();
-    }, time);  // 1 minuto de inactividad
+    inactivityTimeout = setTimeout(showInactivityAlert, time);
 };
 
 socket.onclose = () => {
@@ -50,6 +47,34 @@ function sendMessage() {
     }, 60000);  // 1 minuto de inactividad
 }
 
+function confirmAlert(userConfirmed) { 
+    clearTimeout(alertTimeout); 
+    const alertBox = document.getElementById('alertBox'); 
+    alertBox.style.display = 'none'; 
+    if (userConfirmed) { 
+        clearTimeout(inactivityTimeout); 
+        inactivityTimeout = setTimeout(showInactivityAlert, 60000); 
+        // Reiniciar el temporizador 
+        } 
+    else { 
+        sendCloseSessionMessage(); 
+        socket.close(); 
+        } 
+    }
+
+function showInactivityAlert() { 
+    const alertBox = document.getElementById("alertBox");
+    alertBox.style.display = "block";
+    // iniciar conteo
+    alterTimeout = setTimeout( () => {
+        sendCloseSessionMessage();
+        alertBox.style.display ="none";
+        const responseArea = document.getElementById('responseArea');
+        responseArea.innerHTML += `<p>Coneccion Finalizada por inactividad</p>`;
+        socket.close();
+        }, 30000);
+    } 
+
 function sendCloseSessionMessage() {
     if (session_id) {
         const message = {
@@ -59,6 +84,4 @@ function sendCloseSessionMessage() {
         socket.send(JSON.stringify(message));
     }
 }
-
-let inactivityTimeout;
 
